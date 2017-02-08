4c1b49ec14254171035e6533076cb7250603735a
Brittany Storoz <brittanystoroz@gmail.com>
2017-02-08 04:52:44 -0700

    Update syntax

--- tests/test.js
+++ tests/test.js
@@ -1,9 +1,9 @@
-let Application = require('spectron').Application
-let expect = require('chai').expect;
-let assert = require('chai').assert;
-const path = require('path')
-const chai = require('chai')
-const chaiAsPromised = require('chai-as-promised')
+const Application = require('spectron').Application;
+const chai = require('chai');
+const chaiAsPromised = require('chai-as-promised');
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+const path = require('path');
 
 let electronPath = path.join(__dirname, '..', 'node_modules', '.bin', 'electron');
 let appPath = path.join(__dirname, '..');
@@ -15,39 +15,40 @@ global.before(function () {
 });
 
 describe('App starts and has correct title and buttons', function () {
-  let app = null
+  let app;
+
   before(function () {
       app = new Application({ 
         path: electronPath,
-        env: { SPECTRON: '1' },
+        env: { SPECTRON: true },
         args: [appPath, '-r', mocksPath]
       });
       return app.start();
   });
 
-  after(function (done) {
-      done();
+  after(function () {
       return app.stop();
   });
 
   it('opens a window', function () {
-    return app.client.waitUntilWindowLoaded()
-      .getWindowCount().should.eventually.equal(1);
+    return app.client.waitUntilWindowLoaded().getWindowCount()
+            .should.eventually.equal(1);
   });
 
   it('tests the title', function () {
     return app.client.waitUntilWindowLoaded().getTitle()
             .should.eventually.equal('Fire Sale');
   });
-  it('tests the Open File button text is not disabled', function () {
-    return app.client.waitUntilWindowLoaded().getText('#open-file')
-            .should.eventually.equal('Open File')
-  })
 
-  it('tests the Save button text is disabled', function () {
+  it('tests the Open File button text exists', function() {
+    return app.client.getText('#open-file')
+            .should.eventually.equal('Open File');
+  });
+
+  it('tests the Save button text exists', function () {
     return app.client.getText('#save-markdown')
-            .should.eventually.equal('Save Markdown')
-  })
+            .should.eventually.equal('Save Markdown');
+  });
 
   it('tests the Open button opens a file dialog', function (done) {
     app.client.click('#open-file').then((dialog) => { 
